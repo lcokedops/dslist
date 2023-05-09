72daa03c496e601884632105424838b2d88d8c3d
Leonardo Dias <leonardodias97@gmail.com>
2023-05-08 22:25:16 -0300

    Game repository, service, controller

---
+++ src/main/java/com/devsuperior/dslist/entities/dto/GameMinDTO.java
@@ -0,0 +1,83 @@
+package com.devsuperior.dslist.entities.dto;
+
+import java.util.Objects;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMinDTO {
+	private Long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shortDescription;
+	
+	public GameMinDTO() {
+	}
+
+	public GameMinDTO(Game entity) {
+		this.id = entity.getId();
+		this.title = entity.getTitle();
+		this.year = entity.getYear();
+		this.imgUrl = entity.getImgUrl();
+		this.shortDescription = entity.getShortDescription();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameMinDTO other = (GameMinDTO) obj;
+		return Objects.equals(id, other.id);
+	}
+
+		
+}
